ea498123a11430aa7dc38a2d710cdfc9df5a54d1
yms <mi_msyuan@outlook.com>
2019-01-12 23:32:05 +0800

    get the range of canvas

--- day_03/part_01.js
+++ day_03/part_01.js
@@ -2,62 +2,57 @@ const fs = require('fs')
 const data = fs.readFileSync("./input",{encoding:"utf8"}).split("\n")
 const test = fs.readFileSync("./test",{encoding:"utf8"}).split("\n");
 
-
 const dataXY = data.map(e => {
   const rectangleXY = {};
-  rectangleXY.id = e.split(" ").shift();
-  rectangleXY.X1 = e.split("")[5];
-  rectangleXY.X2 = (e.split("")[10]-0)+(rectangleXY.X1-0);
-  rectangleXY.Y1 = e.split("")[7];
-  rectangleXY.Y2 = (e.split("")[12]-0)+(rectangleXY.Y1-0);
+  rectangleXY.id = e.match(/#[0-9]+/).shift()
+  rectangleXY.X1 = e.match(/@.([0-9]+)/)[1] - 0
+  rectangleXY.X2 = (e.match(/:.([0-9]+)/)[1]-0)+(rectangleXY.X1-0);
+  rectangleXY.Y1 = e.match(/,([0-9]+)/)[1] - 0;
+  rectangleXY.Y2 = (e.match(/x([0-9]+)/)[1]-0)+(rectangleXY.Y1-0);
+  rectangleXY.rangeX = rangeArray(rectangleXY.X1, rectangleXY.X2);
+  rectangleXY.rangeY = rangeArray(rectangleXY.Y1, rectangleXY.Y2);
   return rectangleXY;
-})
+});
+const maxX = getMaxOfArray(dataXY.map(e => e.X2))
+const maxY = getMaxOfArray(dataXY.map(e => e.Y2))
+let canvas = {x:rangeArray(0, maxX),y:rangeArray(0, maxY)}
 
-function getArea(obj1,obj2) {
-  const OneX1 = obj1.X1;
-  const OneX2 = obj1.X2;
-  const OneY1 = obj1.Y1;
-  const OneY2 = obj1.Y2;
-  const TwoX1 = obj2.X1;
-  const TwoX2 = obj2.X2;
-  const TwoY1 = obj2.Y1;
-  const TwoY2 = obj2.Y2;
-  let result = 0;
-  // obj2 包含 obj1
-  if( (OneX1 <= TwoX1 && OneX2 <= TwoX2) && (OneY1 <= TwoY1 && OneY2 <= TwoY2) ){
-    result = (OneX2 - OneX1) * (OneY2 - OneY1);
-  }
-  // obj1 包含 obj2
-  if((TwoX1 <= OneX1 && TwoX2 <= OneX2) && (TwoY1 <= OneY1 && TwoY2 <= OneY2)){
-    result = (TwoX2 - TwoX1) * (TwoY2 - TwoY1);
-  }
-  // obj1 与 obj2 有交集
-  if (OneX1 < TwoX1 && TwoX1 < OneX2 && OneY1 < TwoY1 && TwoY1 < OneY2) {
-    result = (OneX2-TwoX1) * (OneY2-TwoY1)
-  }
-  if (OneX1 < TwoX1 && TwoX1 < OneX2 && TwoY1 < OneY1 && OneY1 < TwoY2) {
-    result = (OneX2-TwoX1) * (TwoY2-OneY1)
-  }
-  if (TwoX1 < OneX1 && OneX1 < TwoX2 && OneY1 < TwoY1 && TwoY1 < OneY2) { 
-    result = (TwoX2-OneX1) * (OneY2-TwoY1)
-  }
-  if (TwoX1 < OneX1 && OneX1 < TwoX2 && TwoY1 < OneY1 && OneY1  < TwoY2) { 
-    result = (TwoX2-OneX1) * (TwoY2-OneY1)
-  }
-  // obj1 与 obj2 没有交集
-  if(OneX2 < TwoX1 || TwoX2 < OneX1){
-    result = 0;
+function getMaxOfArray(numArray) {
+  return Math.max.apply(null, numArray);
+}
+function rangeArray (start, end) {
+  return Array(end - start + 1).fill(0).map((v, i) => i + start);
+}
+function getArrayRepeat(arr1, arr2) {
+  const result = [];
+  for(let i = 0; i < arr1.length; i++){
+    if(arr2.indexOf(arr1[i]) !== -1) result.push(arr1[i]);
   }
   return result;
 }
-
+//去掉画布中矩形重合的部分  并算出重合面积
+function getRepeatArea(obj1, obj2) {
+  const ArrayX = getArrayRepeat(obj1.rangeX, obj2.rangeX);
+  const ArrayY = getArrayRepeat(obj1.rangeY, obj2.rangeY);
+  const aim = {x:[], y:[]};
+  // 画布中存在
+  canvas.x.map(e => {
+   if(ArrayX.indexOf(e) === -1) aim.x.push(e);
+   return aim;
+  })
+  canvas.x.map(e => {
+   if(ArrayY.indexOf(e) === -1) aim.y.push(e);
+   return aim;
+  })
+  canvas = aim;
+  return (ArrayX.length-1)*(ArrayY.length-1);
+}
 
 let AreaResult=0;
 for(let i = 0; i < dataXY.length; i ++) {
   let j = i;
   for(let j = i+1; j < dataXY.length; j++) {
-    AreaResult += getArea(dataXY[i],dataXY[j]);
+    AreaResult += getRepeatArea(dataXY[i], dataXY[j]);
   }
 }
-
-console.log(AreaResult)
+console.log(AreaResult);
