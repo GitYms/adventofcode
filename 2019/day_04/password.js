a2a6cbffc9d04a2d3ed0c2ce69969c97e10933ce
yms <mi_msyuan@outlook.com>
2019-12-12 17:30:33 +0800

    day4 init

---
+++ 2019/day_04/password.js
@@ -0,0 +1,17 @@
+/*
+  input: 给定密码范围
+  part_1:
+    rules: 
+      1. 密码为给定范围内的6位数；
+      2. 至少有一个相邻的数重复，且数字从左到右只能增加
+    result: 密码可能有多少个
+  part_1:
+    note:
+    result: 
+*/
+const input = [367479, 893698];
+
+// part_1
+function getNumber(min, max) {
+  // 重复6个
+}
