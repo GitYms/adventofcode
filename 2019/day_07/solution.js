99f21f9ab7988848309dcf3b5333b5f88526d356
yms <mi_msyuan@outlook.com>
2019-12-18 17:31:34 +0800

    day7 part2 init

--- 2019/day_07/solution.js
+++ 2019/day_07/solution.js
@@ -51,11 +51,11 @@ function processOutput(input, data) {
   }
   return output;
 }
-const test1 = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0];
+const test1 = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]; // 43210
 const test2 = [3,23,3,24,1002,24,10,24,1002,23,-1,23,
-  101,5,23,23,1,24,23,23,4,23,99,0,0];
+  101,5,23,23,1,24,23,23,4,23,99,0,0]; // 54321
 const test3 = [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,
-  1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0];
+  1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]; // 65210
 function getSingal(sequences, data) {
   const outputArr = [0];
   while(sequences.length > 0) {
@@ -67,6 +67,7 @@ function getSingal(sequences, data) {
   return outputArr[outputArr.length - 1];
 }
 function getSequences(numbers) {
+  const sequences = [];
   for(let i = 0; i < numbers.length; i++) {
     const sencordNumbers = numbers.filter(e => e !== numbers[i]);
     for(let j = 0; j < sencordNumbers.length; j++) {
@@ -83,6 +84,7 @@ function getSequences(numbers) {
       }
     }
   }
+  return sequences;
 }
 function getMaxSingal(data) {
   // 获取序列sequences 【0，1，2，3，4】的任意组合
@@ -97,4 +99,17 @@ console.log('answer1:', answer1)
 
 
 // part 2
-
+const testData1 = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+  27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]; // 139629729
+const testData2 = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+  -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+  53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]; // 18216
+function getCircleMaxSingal(data) {
+  const sequences = getSequences([9, 8, 7, 6, 5]);
+  // 循环sequences -> 循环processOutput 直到没有输出 ->得到signal;
+  const singals = sequences.map(sequence => {
+    // processOutput 输出：【n】
+  })
+}
+const answer2 = getCircleMaxSingal(data);
+console.log('answer2:', answer2)
