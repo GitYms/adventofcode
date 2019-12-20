63394222ebf2a9423c285d820a9a8254459351e6
yms <mi_msyuan@outlook.com>
2019-12-20 13:56:38 +0800

    opt

--- 2019/day_07/solution.js
+++ 2019/day_07/solution.js
@@ -2,6 +2,16 @@ const fs = require('fs');
 const resolveOpcode = require('../common/resolveOpcode');
 const data = fs.readFileSync('./input', 'utf8').split(',').map(e => Number(e));
 
+const part1_test1 = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]; // [4,3,2,1,0] -> 43210
+const part1_test2 = [3,23,3,24,1002,24,10,24,1002,23,-1,23,
+  101,5,23,23,1,24,23,23,4,23,99,0,0]; // [0,1,2,3,4] -> 54321
+const part1_test3 = [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,
+  1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]; // [1,0,4,3,2] -> 65210
+const part2_test1 = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+  27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]; // [9,8,7,6,5] -> 139629729
+const part2_test2 = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+  -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+  53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]; // [9,7,8,5,6] -> 18216
 // part 1
 function processOutput(input, data) {
   const arr = [...data]
@@ -30,6 +40,7 @@ function processOutput(input, data) {
     if(opcode === 4) {
       output.push(paramA);
       i = i + 2;
+      // return paramA
     }
     if(opcode === 5) {
       paramA !== 0 ? i = paramB : i = i + 3;
@@ -46,19 +57,12 @@ function processOutput(input, data) {
       i = i + 4;
     }
     if(opcode === 99) {
-      console.log(99)
       return output;
     }
   }
   // return output;
 }
-const test1 = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]; // 43210
-const test2 = [3,23,3,24,1002,24,10,24,1002,23,-1,23,
-  101,5,23,23,1,24,23,23,4,23,99,0,0]; // 54321
-const test3 = [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,
-  1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]; // 65210
 function getSingal(sequences, data, init = 0) {
-  // 4363425
   const outputArr = [init];
   while(sequences.length > 0) {
     const sequence = sequences.shift();
@@ -66,7 +70,6 @@ function getSingal(sequences, data, init = 0) {
     const result = processOutput(input, data);
     outputArr.push(result[0])
   }
-  // console.log('getSingal outputArr', outputArr)
   return outputArr;
 }
 function getSequences(numbers) {
@@ -103,18 +106,69 @@ function getMaxSingal(data) {
 // const answer1 = getMaxSingal(data);
 // console.log('answer1:', answer1)
 
-
 // part 2
-const testData1 = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
-  27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]; // 139629729
-const testData2 = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
-  -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
-  53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]; // 18216
-function getCircleSingal(sequence, data) {
-  // 4363425  139629729
-    singals = getSingal(sequence, data, 0);
-    console.log(singals)
-   // return singals[singals.length - 1];
+
+function processOutput2(data, input, i = 0) {
+  console.log('i', i)
+  const arr = [...data]
+  const output = [];
+  for(i; i < arr.length;) {
+    const { opcode, varA, varB, varC } = resolveOpcode(arr[i]);
+    const index1 = varA === 0 ? arr[i+1] : i+1;
+    const index2 = varB === 0 ? arr[i+2] : i+2;
+    const index3 = varC === 0 ? arr[i+3] : i+3;
+    const paramA = arr[index1];
+    const paramB = arr[index2];
+    if(opcode === 1) {
+      arr[index3] = (paramA - 0) + (paramB - 0);
+      i = i + 4;
+    }
+    if(opcode === 2) {
+      arr[index3] = (paramA - 0) * (paramB - 0);
+      i = i + 4;
+    }
+    if(opcode === 3) {
+      arr[index1] = input;
+      i = i + 2;
+    }
+    if(opcode === 4) {
+      output.push(paramA);
+      console.log('param A', paramA)
+      i = i + 2;
+      return processOutput2(arr, paramA, i);
+    }
+    if(opcode === 5) {
+      paramA !== 0 ? i = paramB : i = i + 3;
+    }
+    if(opcode === 6) {
+      paramA === 0 ? i = paramB : i = i + 3;
+    }
+    if(opcode === 7) {
+      paramA < paramB ? arr[index3] = 1 : arr[index3] = 0;
+      i = i + 4;
+    }
+    if(opcode === 8) {
+      paramA === paramB ? arr[index3] = 1 : arr[index3] = 0;
+      i = i + 4;
+    }
+    if(opcode === 99) {
+      console.log('when 99, output', output)
+      return '';
+    }
+  }
+}
+processOutput2(part1_test1, 0, 0)
+function getCircleSingal(sequences, data, init = 0) {
+  // E->A->B->C->D->E 5个放大器
+  // A input:[sequence[0], e]
+  const outputArr = [init];
+  while(sequences.length > 0) {
+    const sequence = sequences.shift();
+    const input = [sequence, outputArr[outputArr.length - 1]];
+    const result = processOutput(input, data);
+    outputArr.push(result[0])
+  }
+  return outputArr;
 }
 function getCircleMaxSingal(data) {
   const sequences = getSequences([9, 8, 7, 6, 5]);
@@ -124,6 +178,5 @@ function getCircleMaxSingal(data) {
   
   return Math.max(...singals)
 }
-getCircleSingal([9,8,7,6,5], testData1)
 // const answer2 = getCircleMaxSingal(data);
 // console.log('answer2:', answer2)
