de3253950a6d3aea315ae9a104f9e3122741925f
yms <mi_msyuan@outlook.com>
2019-12-23 16:48:48 +0800

    ay9 init

--- 2019/common/resolveOpcode.js
+++ 2019/common/resolveOpcode.js
@@ -1,10 +1,10 @@
 // 解析指令，输出： { 指令，参数（A，B，C)的模式 }
-// 模式：1-立即模式，0-位置模式
+// 模式：1-立即模式，0-位置模式 2-相对模式
 module.exports = function resolveOpcode(opcode) {
   const arr = opcode.toString().split('').map(e => Number(e));
   const leng = 5 - arr.length;
   for(let i = 0; i < leng; i++) {
     arr.unshift(0);
   }
-  return { opcode: Number(arr[3].toString() + arr[4].toString()), varA: arr[2], varB: arr[1], varC: arr[0] };
+  return { opcode: Number(arr[3].toString() + arr[4].toString()), modeA: arr[2], modeB: arr[1], modeC: arr[0] };
 }
--- 2019/day_09/solution.js
+++ 2019/day_09/solution.js
@@ -2,15 +2,21 @@ const intcodeComputer = require('../common/intcodeComputer');
 const fs = require('fs');
 const data = fs.readFileSync('./input', { encoding: 'utf8' }).split(',')
 
-console.log(data.length)
+// console.log(data.length)
 // part_1
-function resolve(data, input, base) {
-
+function resolve(data, base, input) {
+  let result = intcodeComputer({data, input, index:0, base});
+  console.log(result)
+  while(result.end) {
+    result = intcodeComputer(result);
+  }
+  console.log(result)
+  return result;
 }
-const part1_test1 = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99];
-const part1_test2 = [1102,34915192,34915192,7,4,7,99,0]
-const part1_test3 = [104,1125899906842624,99]
-const answer1 = resolve();
+const part1_test1 = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]; // itself
+const part1_test2 = [1102,34915192,34915192,7,4,7,99,0] // -> 1219070632396864
+const part1_test3 = [104,1125899906842624,99]  // -> 1125899906842624
+const answer1 = resolve(part1_test3, 0, 1);
 console.log('answer1:', answer1);
 
 // part_2
