72d1487e22e79b34d844816962f695f4c4436526
yms <mi_msyuan@outlook.com>
2019-12-25 15:42:09 +0800

    part1

--- 2019/common/intcodeComputer.js
+++ 2019/common/intcodeComputer.js
@@ -11,52 +11,51 @@ function getIndex(data, index, base, mode) {
   return result;
 }
 
-module.exports = function intcodeComputer(data, input, base = 0) {
-  const output = [];
-  for(let i = 0; i < data.length;) {
-    const { opcode, modeA, modeB, modeC } = resolveOpcode(data[i]);
-    const index1 = getIndex(data, i+1, base, modeA);
-    const index2 = getIndex(data, i+2, base, modeB);
-    const index3 = getIndex(data, i+3, base, modeC);
-    const paramA = data[index1] || 0;
-    const paramB = data[index2];
-    if(opcode === 1) {
-      data[index3] = (paramA - 0) + (paramB - 0);
-      i = i + 4;
-    }
-    if(opcode === 2) {
-      data[index3] = (paramA - 0) * (paramB - 0);
-      i = i + 4;
-    }
-    if(opcode === 3) {
-      data[index1] = input;
-      i = i + 2;
-    }
-    if(opcode === 4) {
-      output.push(paramA);
-      i = i + 2;
-    }
-    if(opcode === 5) {
-      paramA !== 0 ? i = paramB : i = i + 3;
-    }
-    if(opcode === 6) {
-      paramA === 0 ? i = paramB : i = i + 3;
-    }
-    if(opcode === 7) {
-      paramA < paramB ? data[index3] = 1 : data[index3] = 0;
-      i = i + 4;
-    }
-    if(opcode === 8) {
-      paramA === paramB ? data[index3] = 1 : data[index3] = 0;
-      i = i + 4;
-    }
-    if(opcode === 9) {
-      base += paramA;
-      i = i + 2;
-    }
-    if(opcode === 99) {
-      return output;
-    }
+module.exports = function intcodeComputer({arr, i, input, base = 0, output = []}) {
+  const data = [...arr]
+  const outputArr = [...output];
+  const { opcode, modeA, modeB, modeC } = resolveOpcode(data[i]);
+  const index1 = getIndex(data, i+1, base, modeA);
+  const index2 = getIndex(data, i+2, base, modeB);
+  const index3 = getIndex(data, i+3, base, modeC);
+  const paramA = data[index1] || 0;
+  const paramB = data[index2];
+  if(opcode === 1) {
+    data[index3] = (paramA - 0) + (paramB - 0);
+    i = i + 4;
   }
-  // return output;
+  if(opcode === 2) {
+    data[index3] = (paramA - 0) * (paramB - 0);
+    i = i + 4;
+  }
+  if(opcode === 3) {
+    data[index1] = input;
+    i = i + 2;
+  }
+  if(opcode === 4) {
+    outputArr.push(paramA);
+    i = i + 2;
+  }
+  if(opcode === 5) {
+    paramA !== 0 ? i = paramB : i = i + 3;
+  }
+  if(opcode === 6) {
+    paramA === 0 ? i = paramB : i = i + 3;
+  }
+  if(opcode === 7) {
+    paramA < paramB ? data[index3] = 1 : data[index3] = 0;
+    i = i + 4;
+  }
+  if(opcode === 8) {
+    paramA === paramB ? data[index3] = 1 : data[index3] = 0;
+    i = i + 4;
+  }
+  if(opcode === 9) {
+    base += paramA;
+    i = i + 2;
+  }
+  if(opcode === 99) {
+    return outputArr;
+  }
+  return { data, i: i, input: input, base: base, output: outputArr }
 }
--- 2019/day_09/solution.js
+++ 2019/day_09/solution.js
@@ -3,16 +3,18 @@ const fs = require('fs');
 const data = fs.readFileSync('./input', { encoding: 'utf8' }).split(',')
 
 // part_1
-function resolve(data, base, input) {
-  let result = intcodeComputer(data, input, base);
-  return result;
-}
 const part1_test1 = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]; // itself
 const part1_test2 = [1102,34915192,34915192,7,4,7,99,0] // -> 1219070632396864
 const part1_test3 = [104,1125899906842624,99]  // -> 1125899906842624
-const answer1 = intcodeComputer(part1_test1, 0, 0);
-//  part1_test1 i=12 1006 死循环
-console.log('answer1:', answer1);
-
+function resolve(dataArr, inputValue, baseValue = 0 ) {
+  let result = intcodeComputer({ arr: dataArr, i: 0, inputValue, baseValue });
+  while(!Array.isArray(result)){
+    const { data, i, input, base, output }  = result;
+    result = intcodeComputer({ arr: data, i, input, base, output});
+  }
+  console.log('result', result)   // [203, 0]
+  return result;
+}
+resolve(data, 1)
 // part_2
 
