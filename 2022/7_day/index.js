acb73f2283f32fd27abab5772f1fc711ba03cf14
gitYms <mi_msyuan@outlook.com>
2022-12-18 21:15:04 +0800

    feat: day 7

--- 2022/7_day/index.js
+++ 2022/7_day/index.js
@@ -3,6 +3,8 @@ const input = getInput('./input.txt');
 const input1 = getInput('./input1.txt');
 
 const MAX_SIZE = 100000;
+const SIZE_NEED = 30000000;
+const TOTAL = 70000000;
 /*
   输入： 文件操作指令以及相关结果， dir a(文件夹a),
   输出：
@@ -20,20 +22,70 @@ const getFileNum = (str) => {
   return Number(firtItem);
 }
 
-// console.log('test' , getFileNum('dir sdpjprfb'))
-
 const getNumCanAdd = (num) => (num > MAX_SIZE) ? 0 : num;
 
-const getFileSizeTotal = (input) => {
+const insideFolder = (info) => info.includes('$ cd') && info !== '$ cd ..';
+
+const getFolderSizeArr = (input, folders) => {
+  let size = 0;
+  for(let i = 0; i < input.length; i++) {
+    const info = input[i];
+    // 文件大小
+    size += getFileNum(info);
+    // 内部文件夹大小
+    const isSecondFolder = insideFolder(info);
+    if (isSecondFolder) {
+      let commandNum = 0;
+      let deep = 1;
+      const folderInfo = [];
+      for (let j = i + 1; j < input.length; j++) {
+        commandNum += 1;
+        folderInfo.push(input[j]);
+        if (insideFolder(input[j])) {
+          deep += 1;
+        }
+        if (input[j] === '$ cd ..') {
+          deep -= 1;
+        }
+        if (deep === 0) {
+          break;
+        }
+      }
+      const folderArr = getFolderSizeArr(folderInfo, folders);
+      size += folderArr[folderArr.length - 1];
+      input.splice(i + 1, commandNum);
+    }
+  }
+  folders.push(size);
+  return folders;
+}
+
+const folderSizeArr = getFolderSizeArr(input, []);
+
+// console.log('folderSizeArr', folderSizeArr)
+
+// 获取 没有大于限制的总和
+const getFolderTotal = (folders) => {
   let res = 0;
-  for (let i = 0; i < input.length; i++) {
-    const fileNum = getFileNum(input[i]);
-    // console.log('fileNum', fileNum)
-    const num = getNumCanAdd(fileNum);
-    console.log('num', num)
-    res += num;
+  for (let i = 0; i < folders.length; i++) {
+    res += getNumCanAdd(folders[i]);
   }
   return res;
 }
 
-console.log('part 01: total File size', getFileSizeTotal(input1));
+const getSize = (folders) => {
+  const orderedFolders = folders.sort((a, b) => a - b);
+  const sizeExist = folders.reduce((a, b) => a + b, 0);
+  const needSize = SIZE_NEED - (TOTAL - sizeExist);
+  console.log('orderedFolders', orderedFolders)
+  let total = 0;
+  for (let i = 0; i < orderedFolders.length; i++) {
+    total += orderedFolders[i];
+    if (total >= needSize) {
+      return total;
+    }
+  }
+}
+
+// console.log('part 01: ', getFolderTotal(folderSizeArr));
+console.log('part 02: ', getSize(folderSizeArr));
